c1f7bd577defb6085d16775577b5dea1a0cb4061
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2020-02-17 16:38:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mkdir walk/output
 javac WalkSolution.java
 mv WalkSolution.class walk/
 python walk-to-remember-testcase-gen.py -fnp walk/input/input
-python bash-gen.py -s 0 -e 10 -o walk/gen.sh -l 'java LetterVSolution < input/input{:02d}.txt > output/output{:02d}.txt'
+python bash-gen.py -s 0 -e 10 -o walk/gen.sh -l 'java WalkSolution < input/input{:02d}.txt > output/output{:02d}.txt'
 cd walk
 chmod 700 gen.sh
 ./gen.sh
